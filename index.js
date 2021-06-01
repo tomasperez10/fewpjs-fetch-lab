19f35e7a6f8ebbd9580cbf965cf23eefdc255060
Tomas Perez <tomasitopg6@gmail.com>
2021-06-01 11:00:09 +0000

    needs work

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => console.log(json));
+
+  return fetch();
+
 }
 
 function renderBooks(books) {
